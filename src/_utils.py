68d32bbe0c2cd2530f15a8f113aa6fa318523a0f
Revant Gupta <regu@sh03-04n21.int>
2024-07-16 15:04:16 -0700

    Generalised implementation of edit one hot and ISM.

--- src/_utils.py
+++ src/_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import pyfaidx
 import kipoiseq
+import itertools
 
 # Extract sequence from FASTA file
 # Modified from: https://colab.research.google.com/github/deepmind/\
@@ -41,16 +42,32 @@ def one_hot_encode(sequence):
     return kipoiseq.transforms.functional.one_hot_dna(sequence).astype(np.float32)
 
 # Return all mutations
-def edit_distance_one(seq_onehot):
+def edit_distance_one(seq_onehot, model_window=None):
 
-	if seq_onehot.ndim==2:
-		seq_onehot = np.expand_dims(seq_onehot,0)
+    if seq_onehot.ndim==2:
+        seq_onehot = np.expand_dims(seq_onehot,0)
+    
+    if model_window is not None:
+        seq_shape_ = model_window
+    else:
+        seq_shape_ = seq_onehot.shape[1]
 
-	edited_onehots = np.tile(seq_onehot[0], (seq_onehot.shape[1]*seq_onehot.shape[-1], 1,1))
+    edited_onehots = np.tile(seq_onehot[0], (seq_shape_*seq_onehot.shape[-1], 1,1))
 
-	coords = itertools.product(range(seq_onehot.shape[1]), range(seq_onehot.shape[-1]))
-	for i, (j, k) in enumerate(coords):
-		edited_onehots[i, j, :] = 0
-		edited_onehots[i, j, k] = 1
+    coords = itertools.product(range(seq_shape_), range(seq_onehot.shape[-1]))
+    for i, (j, k) in enumerate(coords):
+        edited_onehots[i, j, :] = 0
+        edited_onehots[i, j, k] = 1
 
-	return edited_onehots
+    return edited_onehots
+
+# Perform ISM with some seq to effect prediction function
+def saturation_mutagenesis(predict_func, model, seq_onehot, 
+                           model_window=2114, batch_size=32, 
+						   **kwargs):
+
+	y0 = predict_func(seq_onehot, model)
+	X_ = edit_distance_one(seq_onehot, model_window)
+	y_hat = predict_func(X_, model=model)
+
+	return y0, y_hat
